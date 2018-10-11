ce9b61e17fa53741e40b9180afd13f9657d49c2c
jcelder <jcelder88@gmail.com>
2018-10-11 12:11:25 -0700

    Set up db connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,11 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var db = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'student',
+  password: 'student'
+});
 
+module.exports = db;
